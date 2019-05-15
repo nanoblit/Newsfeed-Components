b760ecd7ebb92cb677ee1a89126911d6df155840
nanoblit <nanoblit@users.noreply.github.com>
2019-05-15 14:55:56 +0200

    Add buildArticle function

--- Article/Article.js
+++ Article/Article.js
@@ -18,7 +18,7 @@ class Article {
     const closeButton = document.createElement("span");
     closeButton.classList.add("closeButton");
     closeButton.textContent = "Close";
-    closeButton.addEventListener('click', () => this.closeArticle());
+    closeButton.addEventListener("click", () => this.closeArticle());
     this.domElement.append(closeButton);
 
     return closeButton;
@@ -45,6 +45,66 @@ class Article {
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
 */
+
+const articleData = [
+  (dogsArticle = {
+    header: "Dogs",
+    date: "Nov 7th, 2017",
+    "paragraph-1":
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit in nostrum earum? Nihil harum, et quidem nobis blanditiis repellat cumque soluta hic nesciunt quam voluptatibus! Similique provident, repudiandae exercitationem aspernatur cupiditate excepturi dicta voluptatem fugiat eveniet nobis eius sed rem.",
+    "paragraph-2":
+      "2222Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit in nostrum earum? Nihil harum, et quidem nobis blanditiis repellat cumque soluta hic nesciunt quam voluptatibus! Similique provident, repudiandae exercitationem aspernatur cupiditate excepturi dicta voluptatem fugiat eveniet nobis eius sed rem."
+  }),
+  (catsArticle = {
+    header: "Dogs",
+    date: "Nov 7th, 2017",
+    "paragraph-1":
+      "3333Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit in nostrum earum? Nihil harum, et quidem nobis blanditiis repellat cumque soluta hic nesciunt quam voluptatibus! Similique provident, repudiandae exercitationem aspernatur cupiditate excepturi dicta voluptatem fugiat eveniet nobis eius sed rem.",
+    "paragraph-2":
+      "4444Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit in nostrum earum? Nihil harum, et quidem nobis blanditiis repellat cumque soluta hic nesciunt quam voluptatibus! Similique provident, repudiandae exercitationem aspernatur cupiditate excepturi dicta voluptatem fugiat eveniet nobis eius sed rem.",
+    "paragraph-3":
+      "5555Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit in nostrum earum? Nihil harum, et quidem nobis blanditiis repellat cumque soluta hic nesciunt quam voluptatibus! Similique provident, repudiandae exercitationem aspernatur cupiditate excepturi dicta voluptatem fugiat eveniet nobis eius sed rem."
+  })
+];
+
+function buildArticle(data) {
+  const article = document.createElement("div");
+  article.classList.add("article");
+
+  const header = document.createElement("h2");
+  header.textContent = data.header;
+
+  const date = document.createElement("p");
+  date.classList.add("date");
+  date.textContent = data.date;
+
+  let paragraphs = [];
+  for (let i = 1; ; i++) {
+    if (data[`paragraph-${i}`] === undefined) break;
+    paragraphs.push(data[`paragraph-${i}`]);
+  }
+  paragraphs = paragraphs.map(paragraphText => {
+    const element = document.createElement("p");
+    element.textContent = paragraphText;
+    return element;
+  });
+
+  const span = document.createElement("span");
+  span.classList.add("expandButton");
+
+  article.append(header);
+  article.append(date);
+  for (let paragraph of paragraphs) {
+    article.append(paragraph);
+  }
+  article.append(span);
+
+  document.querySelector(".articles").append(article);
+}
+
+for (let article of articleData) {
+  buildArticle(article);
+}
 
 const articles = document.querySelectorAll(".article");
 
