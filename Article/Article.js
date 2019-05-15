1b347abf56f04dc14ceb204cf01059e09c1e465f
nanoblit <nanoblit@users.noreply.github.com>
2019-05-15 14:03:37 +0200

    Add transitions

--- Article/Article.js
+++ Article/Article.js
@@ -7,7 +7,7 @@ class Article {
     // create a reference to the ".expandButton" class.
     this.expandButton = domElement.querySelector(".expandButton");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.expandButton.textContent = "Click to expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener("click", () => this.expandArticle());
   }
@@ -15,6 +15,10 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle("article-open");
+    this.expandButton.textContent =
+      this.expandButton.textContent === "Click to expand"
+        ? "Click to hide"
+        : "Click to expand";
   }
 }
 
